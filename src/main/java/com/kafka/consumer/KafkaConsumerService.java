b9814238f30a26451a2c2185a02f510df5dbc317
laiyefei <laiyf@ebaiyihui.com>
2021-04-14 17:08:58 +0800

    []

---
+++ src/main/java/com/kafka/consumer/KafkaConsumerService.java
@@ -0,0 +1,21 @@
+package com.kafka.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class KafkaConsumerService {
+
+    @KafkaListener(groupId = "group02", topics = "topic02")
+    public void onMessage(ConsumerRecord<String, Object> record, Acknowledgment acknowledgment, Consumer<?, ?> consumer){
+
+        log.info("消费端接收消息:{}", record.value());
+        // 手动签收机制
+        acknowledgment.acknowledge();
+    }
+}
